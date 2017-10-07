5532ca73b7de49d42f01980a1d735da5ac1b0861
SharpeRAD <code@sharperad.com>
2017-10-07 18:52:29 +0100

    Fix unit-tests

--- src/Cake.AWS.CloudFront.Tests/Tests/CloudFrontTests.cs
+++ src/Cake.AWS.CloudFront.Tests/Tests/CloudFrontTests.cs
@@ -1,9 +1,11 @@
 ﻿#region Using Statements
+using System;
 using System.Collections.Generic;
 
 using Xunit;
 using Shouldly;
 
+using Cake.Core;
 using Cake.Core.IO;
 using Cake.AWS.S3;
 using Cake.AWS.CloudFront;
@@ -19,12 +21,19 @@ namespace Cake.AWS.CloudFront.Tests
         public void Test_Invalidation()
         {
             //Sync Directory
-            SyncSettings settings = CakeHelper.CreateEnvironment().CreateSyncSettings();
+            ICakeEnvironment env = CakeHelper.CreateEnvironment();
+
+            DirectoryPath path = new DirectoryPath("./Files/").MakeAbsolute(env.WorkingDirectory);
+            System.IO.File.WriteAllText(path.FullPath + "/Test.txt", Guid.NewGuid().ToString());
+
+
+
+            SyncSettings settings = env.CreateSyncSettings();
             settings.BucketName = "cake-aws-s3";
             settings.KeyPrefix = "cloudfront.tests/utils/";
 
             IS3Manager s3 = CakeHelper.CreateS3Manager();
-            IList<string> keys = s3.SyncUpload(new DirectoryPath("./utils/"), settings);
+            IList<string> keys = s3.SyncUpload(path, settings);
 
             keys.ShouldNotBeEmpty();
 
